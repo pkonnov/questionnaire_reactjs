dc268c45b7baf39e4756c35110c3620709986e1f
Pavel Konnov <4itonada@gmail.com>
2018-11-14 13:34:29 +0400

    add titlePage

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -4,21 +4,34 @@ import AnswersList from '../AnswersList/AnswersList'
 
 
 const ActiveQuiz = props => {
-  const closeTitle = '';
+  let closeTitle;
+  if(props.showTitle){
+    closeTitle =
+      <div>
+      <h1>{props.titleTest}</h1>
+      <p>{props.textTest}</p>
+      <button onClick={props.titleShowHandle}>Начать тест</button>
+      </div>
+  } else {
+    closeTitle =
+      <div>
+        <p className={classes.Question}>
+          <span>
+            <strong>2. </strong>
+            Как дела
+          </span>
+          <small>4 из 10</small>
+        </p>
+
+        <AnswersList
+          answers={props.answers}
+        />
+      </div>
+  }
 
   return (
     <div className={classes.ActiveQuiz}>
-      <p className={classes.Question}>
-        <span>
-          <strong>2. </strong>
-          Как дела
-        </span>
-        <small>4 из 10</small>
-      </p>
-
-      <AnswersList
-        answers={props.answers}
-      />
+        {closeTitle}
     </div>
   )
 }
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,9 +4,9 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component{
   state = {
+    showTitle: true,
     quiz: [
-      { showTitle: true,
-        titleTest: 'Название опросника',
+      { titleTest: 'Название опросника',
         textTest: 'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur", и занялся его поисками в классической латинской литературе. В результате он нашёл неоспоримый первоисточник Lorem Ipsum в разделах 1.10.32 и 1.10.33 книги "de Finibus Bonorum et Malorum" ("О пределах добра и зла"), написанной Цицероном в 45 году н.э. Этот трактат по теории этики был очень популярен в эпоху Возрождения. Первая строка Lorem Ipsum, "Lorem ipsum dolor sit amet..", происходит от одной из строк в разделе 1.10.32',
       },
       {
@@ -20,7 +20,7 @@ class Quiz extends Component{
     ]
   }
 
-  titleShowHandle(){
+  titleShowHandle = () => {
     this.setState({
       showTitle: !this.state.showTitle
     })
@@ -34,6 +34,9 @@ class Quiz extends Component{
             <ActiveQuiz
               answers={this.state.quiz[1].answers}
               showTitle={this.state.showTitle}
+              titleTest={this.state.quiz[0].titleTest}
+              textTest={this.state.quiz[0].textTest}
+              titleShowHandle={this.titleShowHandle}
             />
           </div>
       </div>
